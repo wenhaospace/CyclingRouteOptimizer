4d09795645086abc03c9fa342466ad8d376f1281
wenhao <1925005015@qq.com>
2023-10-07 18:45:34 +0800

    init backend project

---
+++ cyclingapp/src/main/java/com/cro/cyclingapp/CyclingappApplication.java
@@ -0,0 +1,13 @@
+package com.cro.cyclingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyclingappApplication.class, args);
+	}
+
+}
---
+++ cyclingapp/src/test/java/com/cro/cyclingapp/CyclingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cro.cyclingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
