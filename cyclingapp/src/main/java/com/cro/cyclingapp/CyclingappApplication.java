fe1c7b1949c0698ebf504cb654a9966d00001e0c
wenhao <1925005015@qq.com>
2023-10-07 18:59:09 +0800

    add actuator dependency

--- cyclingapp/src/main/java/com/cro/cyclingapp/CyclingappApplication.java
+++ cyclingapp/src/main/java/com/cro/cyclingapp/CyclingappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CyclingappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Cycling!!!");
 		SpringApplication.run(CyclingappApplication.class, args);
 	}
 
